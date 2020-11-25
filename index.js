9fbebbbbd30c688286a84ed37b215943fc9fc8a7
dixen17 <66250773+dixen17@users.noreply.github.com>
2020-11-25 18:44:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const google = require('./commands/google')
+const youtube = require('./commands/youtube')
+const play = require('./commands/play')
+
+bot.on('message', function (message){
+
+if (play.match(message)) {
+return play.action(message)
+}
+
+if (google.match(message)) {
+return google.action(message)
+}
+
+if (youtube.match(message)) {
+return youtube.action(message)
+}
+if (message.content === ('Mollusque mono-neuronal'))
+message.reply('⚠⚠⚠attention a ton language⚠⚠⚠')
+if (message.content === 'dixen')
+message.channel.send('c est le meilleur (*ou presque*)')
+
+bot.on('ready', function () {
+bot.user.setActivity('Minecraft ou à KSP').catch(console.error)
+})
+
+bot.on('gildMemberAdd', function (member){
+member.createDM().then(function (channel){
+channel.send('Bienvenu' + member.displayName)
+})
+})
+bot.login('NzAwNDE3MTUwMzI1ODgyODkw.XpioTQ.5FP0XbWvHQ54a_4FALBqMRV5yYs')
